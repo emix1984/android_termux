cb4c4bd0bad4ae72c2dc23cbaddeba0aafda909c
emix1984 <2909755020@qq.com>
2025-05-14 21:50:51 +0900

    删除更新软件源部分代码

--- system_init.sh
+++ system_init.sh
@@ -20,32 +20,6 @@ configure_storage_permission() {
     fi
 }
 
-# 更新包管理器
-update_package_manager() {
-    echo "更换清华镜像源..."
-    sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/apt/termux-main stable main@' $PREFIX/etc/apt/sources.list
-    
-    if [ $? -ne 0 ]; then
-        echo "镜像源替换失败"
-        exit 1
-    fi
-    
-    echo "更新包管理器..."
-    pkg update -y
-    if [ $? -ne 0 ]; then
-        echo "包管理器更新失败"
-        exit 1
-    fi
-    
-    # 新增升级操作
-    echo "升级所有软件包..."
-    pkg upgrade -y
-    if [ $? -ne 0 ]; then
-        echo "软件包升级失败"
-        exit 1
-    fi
-}
-
 # 安装常用软件包
 install_common_packages() {
     echo "安装常用软件包..."
@@ -72,13 +46,6 @@ install_and_enable_ssh() {
     pkg install openssh -y
     if [ $? -ne 0 ]; then
         echo "OpenSSH 安装失败"
-        exit 1
-    fi
-
-    echo "启用并启动 OpenSSH 服务..."
-    sv-enable sshd
-    if [ $? -ne 0 ]; then
-        echo "OpenSSH 服务启用失败，请手动运行 sv-enable sshd"
         exit 1
     fi
 
@@ -153,13 +120,13 @@ clean_pkg_cache() {
 main() {
     check_termux_environment
     configure_storage_permission
-    update_package_manager
     install_common_packages
     install_and_enable_ssh
     config_pythondev
     clean_pkg_cache  # 新增清理模块
     
     echo "初始化完成！存储剩余空间：$(df -h $PREFIX | awk 'NR==2{print $4}')"
+    echo "当前用户名：$(whoami)"
 }
 
 # 执行主函数
